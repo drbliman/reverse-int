cadb200ce9fb7d9eebe0e68ab9911f4e8f53c527
drbliman <dr.bliman505@gmail.com>
2023-08-01 12:51:56 +0400

    feat: add Solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('');
+    if (arr[0] == '-') {
+        arr.shift()
+    }
+    arr = arr.reverse('');
+    arr = Number(arr.join(''));
+    return arr
 }
